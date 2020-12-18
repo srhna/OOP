b6db17fdeff704ebec1b2e2ca4b7847c14d22817
srhna <76193312+srhna@users.noreply.github.com>
2020-12-18 11:07:30 +0700

    Create enkapsulasi(priv).php

---
+++ enkapsulasi(priv).php
@@ -0,0 +1,21 @@
+<?php
+
+//class kucing
+class kucing {      
+   //menentukan property dengan private
+   private $nama = "inem";             
+   
+   //method public
+   public function tampilkan_nama(){
+          return "nama kucingnya " .$this-nama>;
+   }
+
+}
+//instansiasi class kucing
+$kucing = new kucing();
+
+//memanggil method public tampilkan_nama dari class kucing
+echo $kucing->tampilkan_nama();
+
+
+?>
