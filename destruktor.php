3250ac0a757fa4fe7baf8677867e01d1f35d24fc
srhna <76193312+srhna@users.noreply.github.com>
2020-12-18 10:56:57 +0700

    Create destruktor.php

---
+++ destruktor.php
@@ -0,0 +1,34 @@
+  
+<?php
+
+//class kucing
+class kucing {      
+   //property 
+   var $jenis;              
+   var $warna_bulu;         
+   var $makanan;      
+   
+   //method construct di jalankan pertama kali
+   function __construct(){
+          echo "ini adalah method construct <br/>";
+   }
+   
+   //method destruct di jalankan terakhir
+   function __destruct(){
+          echo "ini adalah method destruct <br/>";
+   }
+   
+   //method kucing 
+   function berdiri(){
+          return "kucingnya berdiri <br/>";
+   }
+
+}
+//instansiasi class kucing
+$kucing = new kucing();
+
+//memanggil method berdiri dari class kucing
+echo $kucing->berdiri();
+
+
+?>
